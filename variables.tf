2b293699f73a8ae4add26cb478c4cf25ecbadd6f
T-helper <botogen@gmail.com>
2022-07-08 14:58:41 +0300

    Update

--- variables.tf
+++ variables.tf
@@ -6,19 +6,12 @@ variable "name" {
   type = string
 }
 
-
 variable "region" {
   type = string
 }
 
-
 variable "machine_type" {
   type = string
-}
-
-variable "target_size" {
-  type    = number
-  default = 1
 }
 
 variable "token" {
