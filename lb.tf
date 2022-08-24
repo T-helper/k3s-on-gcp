3f36ab3ac04ba8bb7cede4d311feacbd278a7abf
T-helper <botogen@gmail.com>
2022-08-24 20:17:10 +0300

    Update

--- lb.tf
+++ lb.tf
@@ -45,7 +45,7 @@ resource "google_compute_forwarding_rule" "k3s-api-server-internal" {
 resource "google_compute_region_backend_service" "k3s-api-server-external" {
   name                  = "k3s-api-server-external"
   region                = var.region
-  load_balancing_scheme = "INTERNAL"
+  load_balancing_scheme = "EXTERNAL"
   health_checks         = [google_compute_region_health_check.k3s-health-check-external.id]
   backend {
     group = google_compute_region_instance_group_manager.k3s-servers.instance_group
@@ -55,7 +55,7 @@ resource "google_compute_region_backend_service" "k3s-api-server-external" {
 resource "google_compute_forwarding_rule" "k3s-api-server-external" {
   name                  = "k3s-api-server-external"
   region                = var.region
-  load_balancing_scheme = "INTERNAL"
+  load_balancing_scheme = "EXTERNAL"
   ip_address            = google_compute_address.k3s-api-server-external.address
   backend_service       = google_compute_region_backend_service.k3s-api-server-external.id
   port_range            = "6443-6443"
