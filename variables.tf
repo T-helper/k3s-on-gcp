aa0511a6022e69dda1788baa98490bb28ee1f24c
T-helper <botogen@gmail.com>
2022-08-26 18:15:54 +0300

    Added agents-v1

--- variables.tf
+++ variables.tf
@@ -2,18 +2,35 @@ variable "project" {
   type = string
 }
 
+variable "name" {
+  type = string
+}
+
+variable "network" {
+  type = string
+}
+
 variable "region" {
   type = string
 }
 
-variable database {
-  type = map
+variable "cidr_range" {
 }
 
-variable servers {
-  type = map
+variable "machine_type" {
+  type = string
 }
 
-variable agents {
-  type = map
+variable "target_size" {
+  type    = number
+  default = 3
+}
+
+variable "token" {
+}
+
+variable "server_address" {
+}
+
+variable "service_account" {
 }
