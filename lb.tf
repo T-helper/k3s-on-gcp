3949801697e157ee3b5755bc3a8bc789af4fc1a9
T-helper <botogen@gmail.com>
2022-08-30 00:55:06 +0300

    Update

---
+++ lb.tf
@@ -0,0 +1,30 @@
+resource "google_compute_region_health_check" "k3s-health-check-ingress" {
+  name   = "k3s-health-check-ingress"
+  region = var.region
+
+  timeout_sec        = 1
+  check_interval_sec = 5
+
+  tcp_health_check {
+    port = 6443
+  }
+}
+
+resource "google_compute_region_backend_service" "k3s-ingress-external" {
+  name                  = "k3s-ingress-external"
+  region                = var.region
+  load_balancing_scheme = "EXTERNAL"
+  health_checks         = [google_compute_region_health_check.k3s-health-check-ingress.id]
+  backend {
+    group = google_compute_region_instance_group_manager.k3s-servers.instance_group
+  }
+}
+
+resource "google_compute_forwarding_rule" "k3s-ingress-external" {
+  name                  = "k3s-ingress-external"
+  region                = var.region
+  load_balancing_scheme = "EXTERNAL"
+  ip_address            = google_compute_address.k3s-api-server-external.address
+  backend_service       = google_compute_region_backend_service.k3s-api-server-external.id
+  port_range            = "6443-6443"
+}
