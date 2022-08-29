ebc8d6965b444724a1c5a9cf73fba9d687cf3a6c
T-helper <botogen@gmail.com>
2022-08-29 12:30:05 +0300

    Update

--- igm.tf
+++ igm.tf
@@ -15,7 +15,7 @@ data "template_file" "k3s-server-startup-script" {
     db_password            = var.db_password
     project                = var.project
     region                 = var.region
-    instance_id            = lookup(data.google_compute_region_instance_group.data_masters.instances[0], "instance")
+    instance_id            = lookup(data.google_compute_region_instance_group.k3s-servers.instances[0], "instance")
   }
 }
 
