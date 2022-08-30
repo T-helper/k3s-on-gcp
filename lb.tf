b3b736fa654007a64155bfeea479db1635b40438
T-helper <botogen@gmail.com>
2022-08-30 10:48:21 +0300

    Update

--- lb.tf
+++ lb.tf
@@ -26,5 +26,5 @@ resource "google_compute_forwarding_rule" "k3s-ingress-external" {
   load_balancing_scheme = "EXTERNAL"
   ip_address            = google_compute_address.k3s-ingress-external.address
   backend_service       = google_compute_region_backend_service.k3s-ingress-external.id
-  port_range            = "6443-6443"
+  port_range            = "80,443"
 }
