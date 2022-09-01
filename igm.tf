85e1c1cf43acb1974e8c67ea90e0edbe33b5dc8f
T-helper <botogen@gmail.com>
2022-09-01 18:58:53 +0300

    modified:   igm.tf

--- igm.tf
+++ igm.tf
@@ -18,7 +18,7 @@ resource "google_compute_instance_template" "k3s-agent" {
   }
 
   disk {
-    source_image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    source_image = "debian-cloud/debian-11"
     auto_delete  = true
     boot         = true
   }
