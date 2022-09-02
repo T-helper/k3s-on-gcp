778793692c75e828e99bcf5becf12dc8003619e8
T-helper <botogen@gmail.com>
2022-09-02 11:40:13 +0300

    modified:   templates/server.sh

--- templates/server.sh
+++ templates/server.sh
@@ -6,10 +6,12 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.23.8+k3s1" sh -s - \
     --token "${token}" \
     --tls-san "${internal_lb_ip_address}" \
     --tls-san "${external_lb_ip_address}" \
-    --node-taint "CriticalAddonsOnly=true:NoExecute" \
     --disable traefik \
     --datastore-endpoint "postgres://${db_user}:${db_password}@${db_host}:5432/${db_name}" \
     --kube-apiserver-arg "service-node-port-range=80-32767" \
     --flannel-backend ipsec \
-    --docker \
-    --cluster-cidr "172.16.0.0/16"
+    --docker
+
+
+
+    #     --node-taint "CriticalAddonsOnly=true:NoExecute" \
