35fdb76ad72ce0cefae3a90b3bb13773eedefac4
T-helper <botogen@gmail.com>
2022-09-02 07:54:20 +0300

    modified:   templates/server.sh

--- templates/server.sh
+++ templates/server.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 curl https://releases.rancher.com/install-docker/19.03.sh | sh
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.23.8+k3s1" sh -s - \
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=" v1.23.10+k3s1" sh -s - \
     --write-kubeconfig-mode 644 \
     --token "${token}" \
     --tls-san "${internal_lb_ip_address}" \
     --tls-san "${external_lb_ip_address}" \
     --node-taint "CriticalAddonsOnly=true:NoExecute" \
-    --disable traefik \
+    --disable traefik,servicelb  \
     --datastore-endpoint "postgres://${db_user}:${db_password}@${db_host}:5432/${db_name}" \
     --kube-apiserver-arg "service-node-port-range=80-32767" \
     --docker
