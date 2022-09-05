648a65b44e52c93118accc1e38cef858dbb4a2f2
T-helper <botogen@gmail.com>
2022-09-05 11:19:57 +0300

    modified:   igm.tf 	modified:   network.tf

--- igm.tf
+++ igm.tf
@@ -25,7 +25,7 @@ resource "google_compute_instance_template" "k3s-agent" {
 
   network_interface {
     network    = var.network
-    subnetwork = var.subnetwork
+    subnetwork = google_compute_subnetwork.k3s-agents.self_link
   }
 
   shielded_instance_config {
