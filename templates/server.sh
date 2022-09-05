014da6ba679b06ebaac130f561d68e0d234ae49c
T-helper <botogen@gmail.com>
2022-09-05 11:17:13 +0300

    modified:   templates/server.sh

--- templates/server.sh
+++ templates/server.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+apt update && apt install wireguard
 # curl https://releases.rancher.com/install-docker/19.03.sh | sh
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.24.4+k3s1" sh -s - \
@@ -10,7 +11,7 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.24.4+k3s1" sh -s - \
     --disable "traefik,local-storage,metrics-server"\
     --datastore-endpoint "postgres://${db_user}:${db_password}@${db_host}:5432/${db_name}" \
     --kube-apiserver-arg "service-node-port-range=80-32767"
-    --flannel-backend=wireguard
+    --flannel-backend wireguard
     
     # --docker
     # --kube-apiserver-arg "service-node-port-range=80-32767" \
