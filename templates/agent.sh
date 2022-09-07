f10852cdde7957009c11cc90991e36c02cd201d8
T-helper <botogen@gmail.com>
2022-09-07 17:13:52 +0300

    modified:   templates/agent.sh

--- templates/agent.sh
+++ templates/agent.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 curl -sfL https://get.k3s.io | K3S_TOKEN="${token}" INSTALL_K3S_VERSION="v1.24.4+k3s1" K3S_URL="https://${server_address}:6443" sh -s - \
-    --node-label "svccontroller.k3s.cattle.io/enablelb=true"
+    --node-label "svccontroller.k3s.cattle.io/enablelb=true" \
+    --node-taint "VpnAddonsOnly=true:NoExecute"
