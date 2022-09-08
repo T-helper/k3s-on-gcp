d51d720d184245e45f9e04b74a571339454babeb
T-helper <botogen@gmail.com>
2022-09-08 19:35:42 +0300

    modified:   templates/server.sh

--- templates/server.sh
+++ templates/server.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-apt update && apt install wireguard
-# curl https://releases.rancher.com/install-docker/19.03.sh | sh
+apt update && apt install wireguard -y && 
+curl https://releases.rancher.com/install-docker/19.03.sh | sh
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.24.4+k3s1" sh -s - \
     --write-kubeconfig-mode 644 \
