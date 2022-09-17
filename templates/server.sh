be681f0f52a0adad1a440061aaee1e8c7020c954
T-helper <botogen@gmail.com>
2022-09-17 09:52:33 +0300

    Update

--- templates/server.sh
+++ templates/server.sh
@@ -12,4 +12,4 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.24.4+k3s1" sh -s - \
     --datastore-endpoint "mysql://${db_user}:${db_password}@tcp(${db_host}:3306)/${db_name}" \
     --kube-apiserver-arg "service-node-port-range=80-32767" \
     --docker \
-    --flannel-backend wireguard
+    --flannel-backend wireguard-native
