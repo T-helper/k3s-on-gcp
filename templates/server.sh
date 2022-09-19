d0a5e53b078cb0e2c3d77e296c7c592390b19519
T-helper <botogen@gmail.com>
2022-09-19 15:24:51 +0300

    modified:   templates/server.sh

--- templates/server.sh
+++ templates/server.sh
@@ -10,5 +10,5 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.24.4+k3s1" sh -s - \
     --node-taint "CriticalAddonsOnly=true:NoExecute" \
     --disable "traefik,local-storage,servicelb" \
     --datastore-endpoint "mysql://${db_user}:${db_password}@tcp(${db_host}:3306)/${db_name}" \
-    --kube-apiserver-arg "service-node-port-range=80-32767" \
+    --kube-apiserver-arg "service-node-port-range=80-32767, cluster-cidr=172.16.0.0/16, service-cidr=172.17.0.0/16" \
     --docker
