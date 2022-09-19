ccc85f9380bf541611ce3708b31b04deab964b9b
T-helper <botogen@gmail.com>
2022-09-19 17:51:02 +0300

    modified:   templates/server.sh

--- templates/server.sh
+++ templates/server.sh
@@ -2,7 +2,7 @@
 apt update && apt install wireguard -y && 
 curl https://releases.rancher.com/install-docker/19.03.sh | sh
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.24.4+k3s1" sh -s - \
+curl -sfL https://get.k3s.io | sh -s - \
     --write-kubeconfig-mode 644 \
     --token "${token}" \
     --tls-san "${internal_lb_ip_address}" \
@@ -20,3 +20,4 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.24.4+k3s1" sh -s - \
 
 
     # --datastore-endpoint "mysql://${db_user}:${db_password}@tcp(${db_host}:3306)/${db_name}" \
+    #INSTALL_K3S_VERSION="v1.24.4+k3s1" 
