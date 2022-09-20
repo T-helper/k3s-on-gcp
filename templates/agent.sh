ba659eb54795f589014beb52db815821dd0745eb
T-helper <botogen@gmail.com>
2022-09-20 17:44:26 +0300

    modified:   templates/agent.sh

--- templates/agent.sh
+++ templates/agent.sh
@@ -3,5 +3,4 @@ apt update && apt install wireguard socat -y &&
 curl https://releases.rancher.com/install-docker/19.03.sh | sh
 
 curl -sfL https://get.k3s.io | K3S_TOKEN="${token}" INSTALL_K3S_CHANNEL="stable" K3S_URL="https://${server_address}:6443" sh -s - \
-    --node-label "svccontroller.k3s.cattle.io/enablelb=true" \
-    --docker
+    --node-label "svccontroller.k3s.cattle.io/enablelb=true" 
