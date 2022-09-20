1b93790162e80e5fc8caba75fcf4b99c24ddb5d4
T-helper <botogen@gmail.com>
2022-09-20 16:58:19 +0300

    modified:   templates/server.sh

--- templates/server.sh
+++ templates/server.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-apt update && apt install wireguard -y && 
+apt update && apt install wireguard socat -y && 
 curl https://releases.rancher.com/install-docker/19.03.sh | sh
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL="stable" sh -s - \
@@ -7,7 +7,6 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL="stable" sh -s - \
     --token "${token}" \
     --tls-san "${internal_lb_ip_address}" \
     --tls-san "${external_lb_ip_address}" \
-    --node-taint "CriticalAddonsOnly=true:NoExecute" \
     --disable "traefik,local-storage,servicelb" \
     --datastore-endpoint "mysql://${db_user}:${db_password}@tcp(${db_host}:3306)/${db_name}" \
     --kube-apiserver-arg "service-node-port-range=80-32767" \
@@ -17,6 +16,5 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL="stable" sh -s - \
 
 
 
-
-    
+#--node-taint "CriticalAddonsOnly=true:NoExecute" \
     #INSTALL_K3S_VERSION="v1.24.4+k3s1" 
