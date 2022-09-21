b5ece8c586047c21dcc348213e2294f3c42a0dd9
T-helper <botogen@gmail.com>
2022-09-21 11:53:32 +0300

    modified:   templates/agent.sh

--- templates/agent.sh
+++ templates/agent.sh
@@ -5,4 +5,4 @@ curl https://releases.rancher.com/install-docker/19.03.sh | sh
 curl -sfL https://get.k3s.io | K3S_TOKEN="${token}" INSTALL_K3S_CHANNEL="stable" K3S_URL="https://${server_address}:6443" sh -s - \
     --node-label "svccontroller.k3s.cattle.io/enablelb=true" \
     --docker \
-    --flannel-backend=wireguard
+    --flannel-backend wireguard
